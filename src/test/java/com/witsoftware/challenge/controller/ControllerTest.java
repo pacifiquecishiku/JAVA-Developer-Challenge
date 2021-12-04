f856c063eea7e6dc0f9af79303be3381dab27b41
Pacifique <pcishiku@gmail.com>
2021-12-04 08:02:22 -0300

    Adicionando alguns testes do Controller

--- src/test/java/com/witsoftware/challenge/controller/ControllerTest.java
+++ src/test/java/com/witsoftware/challenge/controller/ControllerTest.java
@@ -1,16 +1,20 @@
 package com.witsoftware.challenge.controller;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -39,5 +43,20 @@ public class ControllerTest {
                 .andExpect(status().isOk());
         mockMvc.perform(get("/divide?a=12&b=45"))
                 .andExpect(status().isOk());
+        mockMvc.perform(get("/xxx?a=12&b=45"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deveRetornarErroQuandoOsOperandoEstiveremErrados() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/sum?a=4ugh&b=45"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+        String responseBody = mvcResult.getResponse().getContentAsString();
+
+        Assert.assertTrue(responseBody.contains("\"result\":null"));
+        Assert.assertTrue(responseBody.contains("\"hasError\":true"));
+        Assert.assertTrue(responseBody.contains("4ugh"));
     }
 }
